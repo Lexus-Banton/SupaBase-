eb06f78856f42467a69b958756d069d79e9b89b2
Lexus Banton <lexusbanton.kr@gmail.com>
2022-08-10 13:55:30 -0700

    connected table

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // import functions and grab DOM elements
-
+import { getCharacters } from "./fetch-utils";
+const characterListContainer = document.getElementById('character-list-container');
 // let state
-
+c
 // set event listeners 
   // get user input
   // use user input to update state 
